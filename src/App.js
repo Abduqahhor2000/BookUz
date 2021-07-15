9c715f8aa21f67b9ef821801cbc5fa05ce829679
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-07-15 10:44:45 +0500

    audhiud
    addsadsad

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SignUp from "./Component/SignUp";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+        <SignUp /> 
+    </div>
+  );
+}
+
+export default App;
